452bad107d92b3f6972894bf4f03e352a60d2450
Francisco <francisco@nominapress.es>
2022-11-01 17:12:41 +0100

    chore: apollo provider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,17 +1,24 @@
-import '$/styles/fonts.css';
-import { Layout } from '$/containers/Layouts';
+import { Layout } from '$/containers/Layouts/index';
 import GlobalStyle from '$/styles/global';
 import theme from '$/styles/themes';
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
 import type { AppProps } from 'next/app';
 import { ThemeProvider } from 'styled-components';
+import '$/styles/fonts.css';
 
 export default function App({ Component, pageProps }: AppProps): JSX.Element {
+  const client = new ApolloClient({
+    uri: 'https://api-frontend-challenge.herokuapp.com/graphql',
+    cache: new InMemoryCache(),
+  });
   return (
-    <ThemeProvider theme={theme}>
-      <GlobalStyle />
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </ThemeProvider>
+    <ApolloProvider client={client}>
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </ThemeProvider>
+    </ApolloProvider>
   );
 }
