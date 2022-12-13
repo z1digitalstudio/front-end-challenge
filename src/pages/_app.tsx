04446269d93291f7ba670b72403c91fe153c5d9c
Unai Bolivar <ujbolivar@gmail.com>
2022-12-13 21:17:18 +0100

    Provided the client to my application using ApolloProvider in _app.tsx which is Nextjs’ endpoint to what it should render on browser. Also making use of graphql extension with a graphqlrc.yml file in order to build my queries directly inside of a FetchSongList.graphql file and view the results directly in my IDE.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,17 +1,21 @@
 import '$/styles/fonts.css';
 import { Layout } from '$/containers/Layouts';
+import client from '$/graphql/lib/ApolloClient';
 import GlobalStyle from '$/styles/global';
 import theme from '$/styles/themes';
+import { ApolloProvider } from '@apollo/client';
 import type { AppProps } from 'next/app';
 import { ThemeProvider } from 'styled-components';
 
 export default function App({ Component, pageProps }: AppProps): JSX.Element {
   return (
-    <ThemeProvider theme={theme}>
-      <GlobalStyle />
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </ThemeProvider>
+    <ApolloProvider client={client}>
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </ThemeProvider>
+    </ApolloProvider>
   );
 }
